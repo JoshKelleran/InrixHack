eabefc7e1df9c2bae2c52d0eba3f40e15562af93
r-t-hunter-scu <94246728+r-t-hunter-scu@users.noreply.github.com>
2021-11-13 20:56:10 -0800

    Delete Inrix_Main directory

--- Inrix_Main/getCeds.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://www.googleapis.com/auth/calendar")
-
-print(response.text)
--- Inrix_Main/google_tester.py
+++
@@ -1,5 +0,0 @@
-from get_calendar_events import get_events
-
-if __name__ == '__main__':
-
-    get_events()
